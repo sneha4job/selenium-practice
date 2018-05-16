d98f1da08350175f0f4a7ce314cca4ff871496c6
sneha4job <sneha4job@gmail.com>
2018-05-16 18:20:29 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.udemy.classes;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		/* public - access level
+		 * static -
+		 * void - keyword 
+		 */
+		
+		System.out.println("Hello World");
+
+	}
+
+}
